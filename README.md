40526107747bc919830b7f56055c9b2421b5a218
MattBarnabo <mattbarnabo@gmail.com>
2024-03-12 17:51:26 -0500

    adds sql search syntax to challenge questions

--- README.md
+++ README.md
@@ -6,3 +6,19 @@ Holds the database challenges completed by Alpha 2024 cohort.
 2. "United States" "North America" 9.36352e+06
 3. 121
 4. 37
+5. 143
+6. 92
+7. 27
+8. 15
+9. 15
+10. 5
+11. 5
+12. 1
+13. 1
+14. 1
+15. 1
+16. 1
+17. 1
+18. 1
+19. 66years old
+20. 
--- postgresql-morgan-amir-matt.sql
+++ postgresql-morgan-amir-matt.sql
@@ -28,23 +28,131 @@ FROM country
 WHERE governmentform LIKE '%epublic'
 AND indepyear > 1945
 -- Which countries achieved independence after 1945 and are not some kind of republic? (HINT: 27 entries)
+SELECT name, indepyear, governmentform
+FROM country
+WHERE governmentform NOT LIKE '%epublic'
+AND indepyear > 1945
 -- Which fifteen countries have the lowest life expectancy? (HINT: starts with Zambia, ends with Sierra Leonne)
+SELECT name, lifeexpectancy
+FROM country
+ORDER BY lifeexpectancy
+LIMIT 15;
+
 -- Which fifteen countries have the highest life expectancy? (HINT: starts with Andorra, ends with Spain)
+SELECT name, lifeexpectancy
+FROM country
+WHERE lifeexpectancy IS NOT NULL
+ORDER BY lifeexpectancy DESC
+LIMIT 15;
+
 -- Which five countries have the lowest population density (density = population / surfacearea)? (HINT: starts with Greenland)
+SELECT name, population, surfacearea
+FROM country
+WHERE population > 0
+ORDER BY population / surfacearea 
+LIMIT 5;
+
 -- Which countries have the highest population density?(HINT: starts with Macao)
+SELECT name, population, surfacearea
+FROM country
+WHERE population > 0
+ORDER BY population / surfacearea DESC
+LIMIT 5;
+
 -- Which is the smallest country by area? (HINT: .4)
+SELECT name, surfacearea
+FROM country
+ORDER BY surfacearea 
+LIMIT 1;
+
 -- Which is the smallest country by population? (HINT: 50)?
+SELECT name, population
+FROM country
+WHERE population > 0
+ORDER BY population
+LIMIT 1;
+
 -- Which is the biggest country by area? (HINT: 1.70754e+07)
+SELECT name, surfacearea
+FROM country
+ORDER BY surfacearea DESC
+LIMIT 1;
+
 -- Which is the biggest country by population? (HINT: 1277558000)
+SELECT name, population
+FROM country
+WHERE population > 0
+ORDER BY population DESC
+LIMIT 1;
+
 -- Who is the most influential head of state measured by population? (HINT: Jiang Zemin)
+SELECT name, headofstate, population
+FROM country
+ORDER BY population DESC
+LIMIT 1;
+
 -- Of the countries with the top 10 gnp, which has the smallest population? (HINT: Canada)
+WITH populated_countries AS (
+	SELECT name, population, gnp
+	FROM country
+	WHERE population > 0
+	AND gnp > 0
+	ORDER BY gnp DESC
+	LIMIT 10
+)
+SELECT name, population, gnp
+FROM populated_countries
+ORDER BY population
+LIMIT 1;
+
 -- Of the 10 least populated countries with permanent residents (a non-zero population), which has the largest surfacearea? (HINT: Svalbard and Jan Mayen)
+WITH populated_countries AS (
+	SELECT name, population, surfacearea
+	FROM country
+	WHERE population > 0
+	AND surfacearea > 0
+	ORDER BY population
+	LIMIT 10
+)
+SELECT name, population, surfacearea
+FROM populated_countries
+ORDER BY surfacearea DESC 
+LIMIT 1;
+
+
 -- Which region has the highest average gnp? (HINT: North America)
+SELECT region, gnp
+FROM country
+ORDER BY gnp DESC
+Limit 1;
+
 -- Who is the most influential head of state measured by surface area? (HINT: Elisabeth II)
+SELECT headofstate, SUM(surfacearea)
+FROM country
+GROUP BY headofstate
+ORDER BY sum DESC
+
 -- What is the average life expectancy for all continents?
+SELECT AVG(lifeexpectancy)
+FROM country
+
 -- What are the most common forms of government? (HINT: use count(\*))
+SELECT governmentform, COUNT(*)
+FROM country
+GROUP BY governmentform 
+ORDER by count DESC
+
 -- How many countries are in North America?
+SELECT region, COUNT(name)
+FROM country
+WHERE region= 'North America'
+GROUP BY region 
+
 -- What is the total population of all continents?
+SELECT SUM(population)
+FROM country;
+
+
 -- 🏔 Stretch Goals
 -- Which countries have the letter "z" in the name? How many?
 -- Of the smallest 10 countries by area, which has the biggest gnp? (HINT: Macao)
